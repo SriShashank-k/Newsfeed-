b3f98de995174369a4a7148aa220f2737e9b19c2
DeepthiVelicharla <deepthi.velicharla@unh.edu>
2024-04-16 20:10:04 -0400

    create views for articlepage

--- newsfeedapp/views.py
+++ newsfeedapp/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+import requests
+API_KEY = '1dddff4a40694624917ef6388a945af7'
 
-def home(request):
-    return render(request, 'newsfeedapp/home.html')
+
+# Create your views here.
+def home (request):
+    url = f'https://newsapi.org/v2/top-headlines?country=in&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    articles = data['articles']
+    
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'news_api/home.html', context)
