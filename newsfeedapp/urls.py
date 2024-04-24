83985b6f0cb9a58901471593eb45ce40d706951c
SriShashank-k <Srishashank.katari@unh.edu>
2024-04-24 19:38:31 -0400

    Add urls for authentication

--- newsfeedapp/urls.py
+++ newsfeedapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('article/',views.articles_home,name = 'articles')
+    path('', views.home, name='home'),
+    path('logout/', views.user_logout, name='logout'),
+    path('articles/', views.articles_home, name='articles'),
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
 ]
