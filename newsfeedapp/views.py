002b7914dde3c1d586885acf767ffcbbb173484a
SriShashank-k <Srishashank.katari@unh.edu>
2024-04-23 21:13:03 -0400

    Add articles_home function for categorization in views file

--- newsfeedapp/views.py
+++ newsfeedapp/views.py
@@ -17,6 +17,28 @@ def articles_home (request):
 
     return render(request, 'newsfeedapp/articles_home.html', context)
 
+def articles_home(request):
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+    language = request.GET.get('language')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+    elif category:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?country={language}&apiKey={API_KEY}'
+
+    response = requests.get(url)
+    data = response.json()
+    articles = data['articles']
+
+    context = {
+        'articles': articles
+    }
+
+    return render(request, 'newsfeedapp/articles_home.html', context)
+
 def home(request):
 
     return render(request, 'newsfeedapp/home.html')
